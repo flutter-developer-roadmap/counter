dfc45fae9bb7f6d760f9c1ac44b334e39c05d7b5
Eloyse-Billy <eloysedantas22@gmail.com>
2022-11-16 16:33:28 -0300

    Layout demo + Modficações

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,109 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() {
-    return _HomePageState();
-  }
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
         children: [
-          const Text('Olá mundo!'),
-          const Text('Aprendendo Flutter!'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Text('Texto 1 na Row'),
-              Text('Texto 2 na Row'),
-            ],
-          ),
-          Container(
-            alignment: Alignment.center,
-            color: Colors.green,
-            child: const Text(
-              'Texto 3 no Container',
-              style: TextStyle(
-                color: Colors.white,
-              ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Terra Sagrada',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Roblox',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                )
+              ],
             ),
           ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('30'),
         ],
       ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'Ligar'),
+        _buildButtonColumn(color, Icons.near_me, 'Rota'),
+        _buildButtonColumn(color, Icons.share, 'Compartilhar')
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Terra Sagrada de roblox '
+        'onde roblox é epic '
+        '\nEPIC',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter Layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Layout com algumas mudanças'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/boblox.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w400, color: color),
+          ),
+        ),
+      ],
     );
   }
 }
