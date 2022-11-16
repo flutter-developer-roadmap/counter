0d26e6265aa55b421ad445e78a987d25a5462672
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-16 16:49:59 -0300

    isola main

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() {
-    return _HomePageState();
-  }
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Text('Olá mundo!'),
-          const Text('Aprendendo Flutter!'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Text('Texto 1 na Row'),
-              Text('Texto 2 na Row'),
-            ],
-          ),
-          Container(
-            alignment: Alignment.center,
-            color: Colors.green,
-            child: const Text(
-              'Texto 3 no Container',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
