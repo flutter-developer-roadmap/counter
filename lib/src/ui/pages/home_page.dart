8a35569c7d1b73e2a046f67e6a43877781daa624
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-21 17:02:56 -0300

    adiciona botões

--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_button.dart';
 import '../widgets/custom_container.dart';
 
 class HomePage extends StatefulWidget {
@@ -50,6 +54,64 @@ class _HomePageState extends State<HomePage> {
           const CustomContainer(
             text: 'Text Mutável 3',
             backgroundColor: Colors.green,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              log('botão pressionado');
+            },
+            child: const Text('Press'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              log('botão pressionado');
+            },
+            child: const Text('Press'),
+          ),
+          CustomButton(
+            onPressed: () {
+              log('botão pressionado');
+            },
+            text: 'Press',
+          ),
+          IconButton(
+            onPressed: () {
+              log('botão pressionado');
+            },
+            icon: const Icon(Icons.person),
+          ),
+          FloatingActionButton.extended(
+            label: const Text("botão extendido"),
+            onPressed: () {
+              log('botão pressionado');
+            },
+          ),
+          InkWell(
+            highlightColor: Colors.yellow,
+            borderRadius: BorderRadius.circular(15.0),
+            onTap: () {
+              log('botão pressionado');
+            },
+            child: Container(
+              // decoration: BoxDecoration(
+              //   color: Colors.red,
+              //   borderRadius: BorderRadius.circular(15.0),
+              // ),
+              padding: const EdgeInsets.all(8.0),
+              child: const Text("Botão"),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              log("tap");
+            },
+            child: Container(
+              // decoration: BoxDecoration(
+              //   color: Colors.red,
+              //   borderRadius: BorderRadius.circular(15.0),
+              // ),
+              padding: const EdgeInsets.all(8.0),
+              child: const Text("Botão"),
+            ),
           ),
         ],
       ),
