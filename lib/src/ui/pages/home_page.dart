cfede99d5604bd5a80210e41032738664fa1573d
Kaio C. de Oliveira <hello@kaio.dev>
2022-12-21 17:28:58 -0300

    feat: adiciona counter notifier

--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,4 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,12 +14,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ValueNotifier<int> counterNotifier = ValueNotifier(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: const [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            child: ValueListenableBuilder(
+              valueListenable: counterNotifier,
+              builder: (context, state, child) {
+                return Text(
+                  'Contador: $state',
+                  style: const TextStyle(fontSize: 32.0),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterNotifier.value = counterNotifier.value + 1;
+
+          log(counterNotifier.value.toString());
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
