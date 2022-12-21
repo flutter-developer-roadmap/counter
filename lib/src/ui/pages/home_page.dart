6d0ae6ef3bc15aac8627e655536d12ef34038261
Kaio C. de Oliveira <hello@kaio.dev>
2022-12-21 16:57:35 -0300

    update home page

--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,10 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-
-import '../widgets/custom_button.dart';
-import '../widgets/custom_container.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,99 +16,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const CustomContainer(
-            text: 'Olá mundo!',
-            backgroundColor: Colors.grey,
-          ),
-          const CustomContainer(
-            text: 'Aprendendo Componentização!',
-            backgroundColor: Colors.pink,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              CustomContainer(
-                text: 'Texto 1 na Row',
-                backgroundColor: Colors.red,
-              ),
-              CustomContainer(
-                text: 'Texto 2 na Row',
-                backgroundColor: Colors.red,
-              ),
-            ],
-          ),
-          const CustomContainer(
-            text: 'Text Mutável',
-            backgroundColor: Colors.red,
-          ),
-          const CustomContainer(
-            text: 'Text Mutável 2',
-            backgroundColor: Colors.blue,
-          ),
-          const CustomContainer(
-            text: 'Text Mutável 3',
-            backgroundColor: Colors.green,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              log('botão pressionado');
-            },
-            child: const Text('Press'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              log('botão pressionado');
-            },
-            child: const Text('Press'),
-          ),
-          CustomButton(
-            onPressed: () {
-              log('botão pressionado');
-            },
-            text: 'Press',
-          ),
-          IconButton(
-            onPressed: () {
-              log('botão pressionado');
-            },
-            icon: const Icon(Icons.person),
-          ),
-          FloatingActionButton.extended(
-            label: const Text("botão extendido"),
-            onPressed: () {
-              log('botão pressionado');
-            },
-          ),
-          InkWell(
-            highlightColor: Colors.yellow,
-            borderRadius: BorderRadius.circular(15.0),
-            onTap: () {
-              log('botão pressionado');
-            },
-            child: Container(
-              // decoration: BoxDecoration(
-              //   color: Colors.red,
-              //   borderRadius: BorderRadius.circular(15.0),
-              // ),
-              padding: const EdgeInsets.all(8.0),
-              child: const Text("Botão"),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              log("tap");
-            },
-            child: Container(
-              // decoration: BoxDecoration(
-              //   color: Colors.red,
-              //   borderRadius: BorderRadius.circular(15.0),
-              // ),
-              padding: const EdgeInsets.all(8.0),
-              child: const Text("Botão"),
-            ),
-          ),
-        ],
+        children: const [],
       ),
     );
   }
